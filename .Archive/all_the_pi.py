f3555a4c8ff0902b520547cb1212ce2a01d5297c
als0052 <>
2021-02-16 11:08:46 -0500

    initial commit

---
+++ .Archive/all_the_pi.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# Filename: all_the_pi.py
+
+"""
+Format a string containing an arbitrary number of digits of Pi for output to
+a text file.
+
+Created by als0052
+Created on Unknown
+Updated on 02-16-2021
+"""
+
+from datetime import datetime as dt
+import numpy as np
+import math
+
+
+def pi_digits(x=1000):
+	"""Generate x digits of Pi.
+
+		Shamelessly usurped from StackOverflow
+		https://stackoverflow.com/a/54967370/11895567
+
+		:param x: Number of digits of pi. Default is 1,000
+		:type x: int
+	"""
+	k, a, b, a1, b1 = 2, 4, 1, 12, 4
+	while x > 0:
+		p, q, k = k*k, 2*k + 1, k + 1
+		a, b, a1, b1 = a1, b1, p*a + q*a1, p*b + q*b1
+		d, d1 = a/b, a1/b1
+		while d == d1 and x > 0:
+			yield int(d)
+			x -= 1
+			a, a1 = 10*(a % b), 10*(a1 % b1)
+			d, d1 = a/b, a1/b1
+
+
+def print_digits(col_width=12, x_digits=1000, fn=None, chunk_size=100,
+                 cp=None):
+	"""Print out the digits of pi
+
+		This one I wrote.
+
+		:param col_width: Number of digits/characters
+			in a column. Default is 12. Must be a number
+			greater than or equal to 2.
+		:type col_width: int
+		:param x_digits: The number of digits of pi to
+			calculate. Default is 1000.
+		:type x_digits: int
+		:param fn: The name of the output file.
+			Default is None.
+		:type fn: str, PathLike, None
+		:param chunk_size: Number of lines to write at a time. Default is 100.
+		:type chunk_size: int, None
+		:param cp: Check point. If not none, stop the program and wait for user
+			input when the program is ready to begin writing to file.
+		:type cp: None, int, str, float, bool
+	"""
+
+	# Error Handling
+	if col_width < 2:
+		msg = 'col_width must be an integer greater than or equal to 2'
+		raise ValueError(msg)
+
+	if x_digits > 1100000:
+		# Don't let user print more than 1,100,000 digits
+		msg = f"Cannot operate with {x_digits} digits. That is too much."
+		raise RuntimeError(msg)
+
+	# Default output filename
+	if fn is None:
+		date = dt.now().strftime("%m-%d-%Y_%H%M%S")
+		fn = f'pi_printer_{col_width}x{x_digits}_{date}.txt'
+
+	if np.mod(chunk_size, 10) != 0:
+		print('chunk_size must be a positive multiple of 10')
+		chunk_size = int(math.ceil(abs(chunk_size) / 10.0)) * 10
+
+	digits = [int(n) for n in list(pi_digits(x=x_digits))]
+	left_side = np.array([3, np.nan])
+	right_side = np.array([int(x) for x in digits[1:]])
+
+	# combine left_side and right_side
+	all_pi = np.append(left_side, right_side)
+	number_elements = all_pi.shape
+	number_rows = np.floor_divide(number_elements, col_width)
+
+	if np.mod(number_elements, col_width) != 0:
+		# Add zeros to the end of the array to make it evenly divisible by
+		# elements_per_line
+		missing_elements = np.mod(number_elements, col_width)
+		missing_elements = col_width - missing_elements
+		all_pi = np.append(all_pi, np.zeros(missing_elements))
+	all_pi = all_pi.reshape((number_rows[0] + 1, col_width))
+
+	# add the first line "3.1415926535" to list
+	lines = [str(int(all_pi[0][0])) + "." + "".join(
+		str(int(_)) for _ in all_pi[0][2:])]
+
+	for row in range(1, int(number_rows)):
+		# Add the remaining lines to list
+		lines.append("".join(str(int(_)) for _ in all_pi[row]))
+
+	print(f'\tReady to write to file...')
+
+	if cp is not None:
+		# If anything is passed to the checkpoint variable, ``cp``, wait for
+		# user input before writing to the output file.
+		input('Checkpoint parameter is not ``None``. '
+		      'Press any key to continue. ')
+
+	number_iters = _writer_thing(list_lines=lines, filename=fn,
+	                             chunk_size=chunk_size)
+
+	print(f'Iterations: {number_iters}')
+	return number_iters  # todo: this doesn't work right now
+
+
+def _writer_thing(list_lines, filename, chunk_size, iteration=0):
+	"""Write the digits of pi to an output file
+
+		:param list_lines: A list of the lines to write. The lines are the
+			digits of pi broken up into n-character long lines, where n is a
+			parameter passed to print_digits(...) as `col_width`; default is
+			12 characters long.
+		:type list_lines: list, tuple
+		:param filename: the name of the output file to write the lines to.
+		:type filename: str, PathLike
+		:param chunk_size: The number of lines to write at a time.
+		:type chunk_size: int
+		:param iteration: An integer that keeps track of the recursion of this
+			function. Default is 0.
+		:type iteration: int
+	"""
+	# if len(list_lines) >= chunk_size:
+	# 	_chunk_size = chunk_size
+	# else:
+	# 	_chunk_size = len(list_lines)
+
+	if len(list_lines) < chunk_size:
+		chunk_size = len(list_lines)
+
+	# pi_out = open(file=filename, mode='a')
+	with open(filename, 'a', encoding='utf-8') as pi_out:
+		# for i, _ in enumerate(list_lines[:_chunk_size]):
+		for i, _ in enumerate(list_lines[:chunk_size]):
+			pi_out.write(list_lines.pop(0))
+			pi_out.write("\n")
+			if np.mod(i+1, 10) == 0:
+				pi_out.write("\n")
+	# pi_out.close()
+	print(f'Wrote {chunk_size} lines to file. Saving file...')
+
+	if len(list_lines) > 0:  # recursion case
+		_writer_thing(
+			list_lines=list_lines,
+			filename=filename,
+			chunk_size=chunk_size,
+			iteration=iteration+1)
+	else:  # return control to calling function.
+		return iteration + 1
+
+
+def main(x=1000, chunk_size=500):
+	"""Do all the stuff
+
+		:param x: The number of digits of pi to print. Default is 1,000.
+		:type x: int
+		:param chunk_size: The number of lines to print at a time.
+			Default is 500.
+		:type chunk_size: int
+	"""
+	print("\tRunning...")
+	print(f'\tCalculating {x} digits of pi...')
+	print_digits(col_width=12, x_digits=x, chunk_size=chunk_size)
+
+	print(f'Finished formatting {x} digits of Pi.')
+	return None
+
+
+if __name__ == "__main__":
+	# 1,000,000 digits = 44268.841465473175 seconds
+	# main(x=100000, chunk_size=500)
+	main(x=10000, chunk_size=10)
---
+++ Documents/piday.m
@@ -0,0 +1,96 @@
+%% Find the size of the giant paper spool
+%{
+% Assuming every 11th line is a blank (whitespace) line, 
+% there will be 91,668 lines @ 5mm center-center line spacing
+% thus 458,340mm of paper will be needed
+%
+% Assume:
+% 1 roll of paper = 150 ft
+% roll splices use 5mm of each end (10mm total per roll) (not factored into calculations).
+% 
+%}
+clear,clc,close all
+format compact
+
+%% Fill the spool with 458.34 meters of paper
+% Constants
+rotations = 100;	% 1 rotation = 2*pi() radians
+ft_to_mm = 304.8;	% 1ft = 304.8mm
+paper_per_roll = 150 * ft_to_mm;	% mm
+pi2 = 2*pi();	% 2*pi, dimensionless
+t = 0.1;	% paper thickness, mm
+r_co = 15/2;	% core radius outer, mm
+
+% Preallocation
+r = zeros([rotations, 1]);
+s = zeros([rotations, 1]);
+r_paper = zeros([rotations, 1]);
+theta = zeros([1, rotations]);	% row vector!!
+
+% Initial conditions
+r(1, 1) = r_co;	% radius to outer paper layer, mm
+r_paper(1, 1) = r(1, 1) - r_co;	% thickness of just the layers of paper, mm
+s(1, 1) = 0;	% length of paper, mm
+theta(1) = 0;
+
+% Solve
+i = 2;
+while s <= paper_per_roll*11 %s <= 458340	% 458,340 mm
+	theta(i) = round(theta(i-1) + pi2, 4);	% radians
+	dtheta = round(theta(i) - theta(i-1), 4);	% change in theta, should be 2*pi()
+	r(i, 1) = radius(r(i-1, 1), dtheta, t);	% radius to outermost layer of paper, mm
+	s(i, 1) = s(i-1, 1) + linear_paper_length(r(i, 1), dtheta);	% linear length of paper, mm
+	r_paper(i, 1) = r(i, 1) - r_co;	% thickness of the layers of paper, mm
+	i = i + 1;
+	rotations = i;
+end
+
+if mod(round(s(end) / paper_per_roll, 2), 1) ~= 0
+	% number of whole rolls needed
+	% fractional roll case
+	no_rolls = ceil(s(end) / paper_per_roll);
+else
+	% number of whole rolls needed
+	% integer roll case
+	no_rolls = s(end) / paper_per_roll;
+end
+
+% Output
+fprintf("Rotations:\t%d\n", rotations);
+fprintf("Radius of spool from geometric center:\t%.2f mm\n", r(end));
+fprintf("Radius of spool core:\t%.2f mm\n", r_co);
+fprintf("Thickness of layers of paper:\t%.2f mm\n", r_paper(end));
+fprintf("Linear paper length:\t%.2f m\n", s(end)/1000);
+fprintf("Number of rolls of paper:\t%.0f rolls\n", no_rolls);
+clearvars i pi2 dtheta r_co theta
+
+%% Functions
+function rr = radius(r0, thetai, t)
+	%rr = radius(r0, theta, t)
+	%
+	% Inputs:
+	% r0 = initial radius, mm
+	% thetai = angle of rotation, radians. Must be even intervals of 2*pi().
+	% t = thickness of paper, mm
+	two_pi = round(2*pi(), 4);	
+	if round(mod(thetai, two_pi), 0) ~= 0
+		disp(thetai)
+		disp(mod(thetai, 2*pi()))
+		error('theta must be an integer multiple of 2*pi');
+	end
+	rr = r0 + floor(thetai/two_pi)*t;
+	clearvars thetai r0
+end
+function s = linear_paper_length(ri, thetai)
+	%s = linear_paper_length(r, theta)
+	%
+	% Inputs:
+	% ri = current radius, mm
+	% thetai = angle of rotation, radians. Must be an integer multiple of 2*pi().
+	two_pi = round(2*pi(), 4);
+	if round(mod(thetai, two_pi),0) ~= 0
+		error('theta must be an integer multiple of 2*pi');
+	end
+	s = ri*thetai;	% mm
+	clearvars thetai ri
+end
